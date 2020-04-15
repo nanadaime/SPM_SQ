712e034f129e4952b9882ed83b3a7db07a909e6c
giorg <giorg@DESKTOP-5UQM48R>
2020-04-15 21:29:43 +0300

    ola dika s

---
+++ Pharmacy/src/GUI/Hub.java
@@ -0,0 +1,36 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class Hub {
+	
+	private  JFrame frame;
+	
+	private String title;
+	private int width,height;
+	
+	
+	public Hub(String title,int width,int height) {
+		this.height=height;
+		this.title=title;
+		this.width=width;
+		
+		createDisplay();
+		
+		
+	}
+	
+	public void createDisplay() {
+		
+
+		frame=new JFrame(title);
+		frame.setSize(width,height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+	}
+	
+
+}
